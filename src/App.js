f97df598289ab1c5c861a6c9641317fa0b44b88b
Tanishka Srivastava <taani3001@gmail.com>
2025-09-07 06:09:43 +0530

    removed test route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 import './App.css';
-import Test from './components/test';
 import Background from './components/Background'
 export default function App() {
   return (

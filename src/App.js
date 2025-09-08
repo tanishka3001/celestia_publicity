7475151b967e16e41837f39f689751e25af83a8d
Antra1705 <antra.1705@gmail.com>
2025-09-08 21:19:39 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 import './App.css';
 import AboutUs from './components/aboutUs';
+import FAQ from './components/faq';
 export default function App() {
   return (
       <Router>
         <Routes>
-          <Route path='/' element={<AboutUs />} />
+          <Route path='/' element={<><FAQ /><AboutUs /></>} />
+          <Route path='/faq' element={<FAQ />} />
         </Routes>
       </Router>
   );

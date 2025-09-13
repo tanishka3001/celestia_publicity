a34cd1322ecf38ca25accf40fe9fbe8c6d02d004
Tanishka Srivastava <taani3001@gmail.com>
2025-09-13 23:00:55 +0530

    footer font color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
             >
               CELESTIA
             </h1>
-            <p className="text-sm md:text-lg mt-2 text-[#949494]">
+            <p className="text-sm md:text-lg mt-2 text-[#B7B6B6]">
              A 24 hour Vibe-coding Hackathon
             </p>
-            <p className="text-sm md:text-lg mt-2 text-[#949494] font-[Aeonik]">
+            <p className="text-sm md:text-lg mt-2 text-[#B7B6B6] font-[Aeonik]">
              By <span className="italic">ISTE-VIT</span>
             </p>
           </div>
